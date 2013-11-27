fe217305d377f4b56414fc5034559e4676c484df
Guillermo Winkler <guillermo.winkler@gmail.com>
2013-11-27 12:48:57 -0200

    clj-asterisk 0.2.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-asterisk "0.2.4"
+(defproject clj-asterisk "0.2.5"
   :description "Clojure bindings for Asterisk Manager API"
   :url "http://www.github.com/guilespi/clj-asterisk"
   :license {:name "Eclipse Public License"
